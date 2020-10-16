d54167a03db665b4a778c13e9938405c785f0d0d
Rupesh Kumar Sahu <rupeshksahu7@gmail.com>
2020-10-16 12:56:06 +0530

    flutter app

---
+++ lib/main.dart
@@ -0,0 +1,233 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: _MyApp(),
+    );
+  }
+}
+
+class _MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<_MyApp>{
+  @override
+  Widget build(BuildContext context) {
+    var screenWidth = MediaQuery.of(context).size.width;
+
+    final List<String> tasks = <String>['Cooking', 'Study', 'Walking'];
+
+    return Scaffold(
+
+      body: SafeArea(
+
+        child : Container(
+        child: Column(
+          children: [
+            /*Container(
+              width: screenWidth,
+              height: 56,
+              decoration: BoxDecoration(
+                color: Color(0xFF76C8FF)
+              ),
+              child: Center(
+                child: Text('Tasking', style: TextStyle(fontSize: 21, color: Color(0xFFFFFFFF)),),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(16),
+            width: screenWidth,
+            height: 56,
+              child: Container(
+
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Add Task'
+                  ),
+                ),
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.only(left : 16, right: 16),
+            child : Align(
+              alignment: Alignment.topLeft,
+                child : Text('Tasks', style: TextStyle(fontSize: 28, fontWeight: FontWeight.w800),))
+            ),*/
+            AspectRatio(
+              aspectRatio: 4/2,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Color(0xFFffffff)
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      width: screenWidth,
+                      height: 56,
+                      decoration: BoxDecoration(
+                          color: Color(0xFF76C8FF)
+                      ),
+                      child: Center(
+                        child: Text('Tasking', style: TextStyle(fontSize: 21, color: Color(0xFFFFFFFF)),),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(16),
+                      width: screenWidth,
+                      height: 56,
+                      child: Container(
+
+                        child: Row(
+                          children: [
+                            Container(
+                              width: 300,
+                            child : TextField(
+                              decoration: InputDecoration(
+                                  hintText: 'Add Task'
+                              ),
+                            ),
+                            ),
+                            Container(
+                              width: 24,
+                              height: 24,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Color(0xFFFFB9B9)
+                              ),
+                              child: Icon(Icons.add, color:  Colors.white,)
+                            )
+                          ],
+                      ),
+                      )
+                    ),
+                    Container(
+                        margin: EdgeInsets.only(left : 16, right: 16),
+                        child : Align(
+                            alignment: Alignment.topLeft,
+                            child : Text('Tasks', style: TextStyle(fontSize: 28, fontWeight: FontWeight.w800),))
+                    )
+                  ],
+                ),
+              ),
+            ),
+            AspectRatio(
+              aspectRatio: 1/1,
+              child: Container(
+                  child : ListView(
+                    padding: const EdgeInsets.all(8),
+                    children: <Widget>[
+                      Container(
+                        child: Row(
+                          children: [
+                            Radio(
+                              activeColor: Color(0xFF78FFD6),
+                            ),
+                            Text('Cooking', style: TextStyle(color: Color(0xFF000000), fontSize: 28, fontWeight: FontWeight.w800),)
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Row(
+                          children: [
+                            Radio(
+                              activeColor: Color(0xFF78FFD6),
+                            ),
+                            Text('Cooking', style: TextStyle(color: Color(0xFF000000), fontSize: 28, fontWeight: FontWeight.w800),)
+                          ],
+
+                        ),
+                      ),
+                      Container(
+                        child: Row(
+                          children: [
+                            Radio(
+                              activeColor: Color(0xFF78FFD6),
+                            ),
+                            Text('Study Time', style: TextStyle(color: Color(0xFF000000), fontSize: 28, fontWeight: FontWeight.w800),)
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Row(
+                          children: [
+                            Radio(
+                              activeColor: Color(0xFF78FFD6),
+                            ),
+                            Text('Walking 10 min', style: TextStyle(color: Color(0xFF000000), fontSize: 28, fontWeight: FontWeight.w800),)
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Row(
+                          children: [
+                            Radio(
+                              activeColor: Color(0xFF78FFD6),
+                            ),
+                            Text('Excercise', style: TextStyle(color: Color(0xFF000000), fontSize: 28, fontWeight: FontWeight.w800),)
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Row(
+                          children: [
+                            Radio(
+                              activeColor: Color(0xFF78FFD6),
+                            ),
+                            Text('Cooking', style: TextStyle(color: Color(0xFF000000), fontSize: 28, fontWeight: FontWeight.w800),)
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
+                ),
+              ),
+            Container(
+              height: 140,
+              child: Align(
+                alignment: Alignment.bottomCenter,
+              child: Row(
+                children: [
+                  Expanded(
+                   child: Container(
+                     width: 20,
+                    height: 90,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFFF8B8B)
+                    ),
+                     child: Center(
+                       child : Text('Completed Tasks', style: TextStyle(fontSize: 18, color: Color(0xFFFFFFFF)))
+                     ),
+                   ),
+                  ),
+                  Expanded(
+                    child: Container(
+                        width: 20,
+                        height: 90,
+                        decoration: BoxDecoration(
+                            color: Color(0xFFCBFF70)
+                        ),
+                        child: Center(
+                    child : Text('All Tasks', style: TextStyle(fontSize: 18, color: Color(0xFF000000),)
+                  )
+                    ),
+                  ))
+                ],
+              ),
+            )
+            )
+          ],
+        ),
+      ),
+      )
+    );
+  }
+}
